845353e0617aa613e87ca49288723a188655ffe2
Miguel Jose Albano da Silva <61022078+josemiguel2004@users.noreply.github.com>
2021-07-31 10:09:43 -0300

    Initial commit

---
+++ src/test/java/br/com/miguel/lancamentos/LancamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miguel.lancamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LancamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
