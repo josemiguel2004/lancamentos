183bb850bbf872ae31abc465224cfd59f4e4afab
Miguel Jose Albano da Silva <61022078+josemiguel2004@users.noreply.github.com>
2021-08-13 15:11:46 -0300

    13/08/21
    Get and Set no model em  Categoria

--- src/main/java/br/com/miguel/lancamentos/model/Categoria.java
+++ src/main/java/br/com/miguel/lancamentos/model/Categoria.java
@@ -1,11 +1,31 @@
 package br.com.miguel.lancamentos.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "categoria")
 public class Categoria {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
+
+    private String nome;
+
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
