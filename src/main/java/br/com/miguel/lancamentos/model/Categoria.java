cb8da962a5f7ffc6341b0b5680782ebfafdef380
Etec 244 <e244ti@outlook.com>
2021-08-18 10:42:34 -0300

    18/08/21
    Criação do Repository.

--- src/main/java/br/com/miguel/lancamentos/model/Categoria.java
+++ src/main/java/br/com/miguel/lancamentos/model/Categoria.java
@@ -1,6 +1,7 @@
 package br.com.miguel.lancamentos.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "categoria")
@@ -27,5 +28,18 @@ public class Categoria {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return codigo.equals(categoria.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
     }
 }
